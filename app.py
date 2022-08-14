2daefeb6b437f30a0b6fa229e5737f970b77d024
Mitchell Fagert <mitchell.fagert@gmail.com>
2022-08-14 11:39:12 -0400

    Uploading most recent code updates.

--- app.py
+++ app.py
@@ -1,5 +1,47 @@
-from flask import Flask
+#Importing Dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+#Set Up the Database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+#Reflect the Database
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#Create a session link from Python to our database
+session = Session(engine)
+
+#Set Up Flask
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
